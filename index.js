f5ea12aa033b6bdf67ca306d67e4ee8260c54d34
KVinqT <kevinqt4you@gmail.com>
2024-12-17 14:13:47 +0630

    subscribe listeners to the response events that will be emitted from the server

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 import readline from "node:readline";
-import { EventEmitter } from "node:stream";
+import TaskifyServer from "./terminalServer.js";
+import EventEmitter from "node:events";
 
 const client = new EventEmitter();
+const server = new TaskifyServer(client);
+const cursorPrompt = "taskify> ";
+server.on("response", (response) => {
+  process.stdout.write("\u001B[2J\u001B[0;0f"); // clear the prompt
+  process.stdout.write(response);
+  process.stdout.write(`\n${cursorPrompt}`);
+});
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  prompt: "taskify> ",
+  prompt: cursorPrompt,
 });
 
 rl.prompt();
 
 rl.on("line", (line) => {
   let [command, ...args] = line.trim().split(" ");
-  client.emit("command", command, args); //emitting the command altogether with their arguments
+  client.emit("command", command, args);
 }).on("close", () => {
   console.log("Have a great day! Bye");
   process.exit(0);
